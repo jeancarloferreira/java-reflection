997af79e4d3683e265d01d039bb10aed2e116ddb
Jean Carlo <jean.ferreira@multilaser.com.br>
2023-01-04 13:36:37 -0300

    Inserção de arquivos

---
+++ alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteManipulaAtributos.java
@@ -0,0 +1,22 @@
+package br.com.alura.alurator.playground.reflexao;
+
+import java.lang.reflect.Field;
+
+import br.com.alura.alurator.playground.modelo.Produto;
+
+public class TesteManipulaAtributos {
+
+  public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException {
+    Object produto = new Produto("Produto 1", 20.0, "Marca 1");
+    Class<?> classe = produto.getClass();
+
+    System.out.println(classe.getField("id"));
+
+    for(Field atributo : classe.getDeclaredFields()) {
+      atributo.setAccessible(true);
+      System.out.println(atributo.getName() + ": " + atributo.get(produto));
+    }
+  }
+
+}
+
